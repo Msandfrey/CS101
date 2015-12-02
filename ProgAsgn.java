27df2bec65e2e766c1240ad4d0c9e27cbbb5260d
Msandfrey <Msandfrey@users.noreply.github.com>
2015-12-02 15:27:51 -0800

    Create ProgAsgn.java

---
+++ ProgAsgn.java
@@ -0,0 +1,10 @@
+import java.util.Hashtable;     //java hash table class
+import java.util.Scanner;       //scanner to input usernames
+
+class UIMS{
+  public static void main(String[] args){
+    Hashtable<String, Integer> numeros = new Hashtable<String, Integer>();  //new hashtable
+    
+    System.out.println(numeros.size());
+  }
+}
