53207ae0fce9d60afb68d59033ba03a8823088e0
Msandfrey <Msandfrey@users.noreply.github.com>
2015-12-03 19:16:32 -0800

    Update ProgAsgn.java

--- ProgAsgn.java
+++ ProgAsgn.java
@@ -11,32 +11,42 @@ class UIMS{
 }
 
 class Conversion{
-  public String[] stringtoBitSeq(String s){
+  public int[] stringToBitSeq(String s){
     char[] order=['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','g','h','i','j','k','l',
                    'm','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G',
                    'H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
     String uid= s;
-    array=new int[uid.size()];     //array for positions
-    for(int i=0; i<uid.size();i++){
-      char temp=uid.charAt(i);
-       for(int j=0;j<order.size();j++){
-         if(temp == order[j]){
-           array[i]=j;
-           break;
+    array=new int[uid.size()];             //array for positions
+    for(int i=0; i<uid.size();i++){       //go through each character of userId  
+      char temp=uid.charAt(i);            //store character into temp
+       for(int j=0;j<order.size();j++){   //go through order array
+         if(temp == order[j]){            //compare userId char with the chars in order array
+           array[i]=j;                    //if its a match store its position in array
+           break;                         //break out of smaller for loop
          }
        }
     }
-    /*tmp[]=new int[7];
-    for(int m=0;m<array.size();m++){
-      while (array[m] > 0) {
-        tmp[m++] = array[m] % 2;
-        array[m] /= 2;
+    int[] bs = new int[6*uid.size()];       //create the binary sequence array
+    int index = 0;                        //make an index to go through the bs array
+    for(int m = 0; m <array.size(); m++){ //for each number stored in the array array
+      while(array[m]>0){                  //convert it to binary
+        bs[index++] = array[m]%2;         //and store each 0 and 1 in the bs array
+        array[m] = aray[m]/2;             
+      }
     }
-    binary = new int[6*uid.size()];
-    int k = 0;
-    for (int j = m - 1; j >= 0; j--) {
-        binary[k++] = tmp[j];*/
-    }
-
+    return bs[];                          //finally return the bs array
   }
+  
+  public int[] bitseqToDigiteq(int[] bs, int k){
+    
+  }
+  
+  public int[] bitseqToBigNum(int[] bs, int k){
+    
+  }
+  
+  public int[] numToDigitseq(int n){
+    
+  }
+  
 }
