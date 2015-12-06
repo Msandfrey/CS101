b52e58e32ef2dcad5e355d5512efade38d21c1c6
Msandfrey <Msandfrey@users.noreply.github.com>
2015-12-05 21:46:00 -0800

    Update ProgAsgn.java

--- ProgAsgn.java
+++ ProgAsgn.java
@@ -75,9 +75,9 @@ class SLItemList{
 
 class Conversion{
   public int[] stringToBitSeq(String s){
-    char[] order= new char['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','g','h','i','j','k','l',
+    char[] order= {'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','g','h','i','j','k','l',
                    'm','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G',
-                   'H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+                   'H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
     String uid= s;
     int[] array=new int[uid.length()];             //array for positions
     for(int i=0; i<uid.length();i++){       //go through each character of userId  
